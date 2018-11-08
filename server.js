ff10b2a8de38a1ac36c5c6b7c7270861182a8f07
mrserg <mrhersergik@gmail.com>
2018-11-08 13:52:29 +0300

    edit data array by different methods

--- server.js
+++ server.js
@@ -37,6 +37,22 @@ app.post('/artists', function(req, res){
     res.send(artist);
 })
 
+app.put('/artists/:id', function(req, res){
+    var artist = artists.find(function(artist){
+        return artist.id === Number(req.params.id)
+    });
+    artist.name = req.body.name;
+    //res.send(artist);
+    res.sendStatus(200);
+})
+
+app.delete('/artists/:id', function(req, res){
+    artists = artists.filter(function(artist){
+       return artist.id !== Number(req.params.id) 
+    })
+    res.sendStatus(200);
+})
+
 app.get('/artists/:id', function(req, res){
     console.log(req.params);
     var artist = artists.find(function(artist){
